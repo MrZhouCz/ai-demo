a0bd2743bd183c550b608adc7c14de1864180e43
Jonathan <jonathan.cz.zhou@outlook.com>
2026-03-16 03:07:59 +0800

    1

---
+++ src/main/java/org/example/day1/demo1.java
@@ -0,0 +1,18 @@
+package org.example.day1;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class demo1 {
+
+    public static void main(String[] args) {
+
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("https://ark.cn-beijing.volces.com/api/v3")
+                .apiKey("ce250581-d58a-4580-999e-c003385ae61d")
+                .modelName("deepseek-v3-2-251201")
+                .build();
+
+        String answer = model.chat("Say 'Hello World'");
+        System.out.println(answer);
+    }
+}
